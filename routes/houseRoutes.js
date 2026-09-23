961767447f5e9ceefc934ae5f973557b72116b88
Koteswaramma Bekkam <bekkamkoteswaramma@gmail.com>
2026-09-23 19:16:57 +0530

    Format houseRoutes.js for consistency

--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 
 // test route
-router.get('/test', (req,res) => {
+router.get('/test', (req,res) => { 
     res.send('House route working');
 });
 
