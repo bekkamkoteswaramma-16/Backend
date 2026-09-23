1515d3f5304e95082595ae00e5e26d4a566d4c16
Koteswaramma Bekkam <bekkamkoteswaramma@gmail.com>
2026-09-23 19:17:09 +0530

    Format houseRoutes.js for consistency

--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // test route
 router.get('/test', (req,res) => { 
-    res.send('House route working');
+    res.send('House route working'); 
 });
 
 module.exports = router;
